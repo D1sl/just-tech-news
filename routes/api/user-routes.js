791e95bbbfebb219d8c71a503c7d8142a8591484
D1sl <94911312+D1sl@users.noreply.github.com>
2022-03-14 11:16:55 -0500

    13.1.6

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -74,7 +74,7 @@ router.put('/:id', (req, res) => {
 });
 
 // DELETE /api/users/1
-router.get('/:id', (req, res) => {
+router.delete('/:id', (req, res) => {
     User.destroy({
         where: {
             id: req.params.id
