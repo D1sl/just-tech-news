08739a86abf25d938358473496d9ac1a190630b7
D1sl <94911312+D1sl@users.noreply.github.com>
2022-03-21 21:25:34 -0500

    aaa

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,10 +1,16 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const { Post, User, Vote } = require('../../models');
+const sequelize = require('../../config/connection');
 
 // get all users
 router.get('/', (req, res) => {
   Post.findAll({
-    attributes: ['id', 'post_url', 'title', 'created_at'],
+    attributes: [
+      'id', 
+      'post_url', 
+      'title', 
+      'created_at',
+    [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']],
     order: [['created_at', 'DESC']],
     include: [
       {
@@ -25,7 +31,12 @@ router.get('/:id', (req, res) => {
     where: {
       id: req.params.id
     },
-    attributes: ['id', 'post_url', 'title', 'created_at'],
+    attributes: ['id',
+     'post_url',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
     include: [
       {
         model: User,
@@ -58,6 +69,37 @@ router.post('/', (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
+});
+
+// PUT api/posts/upvote -- upvote a post (this route must be above the update route, otherwise express.js will treat upvote as an id)
+router.put('/upvote', (req, res) => {
+  // create an upvote with the user id of the voter and the post id of the upvoted post
+  Vote.create({
+      user_id: req.body.user_id,
+      post_id: req.body.post_id
+  })
+  // find the post just voted on and return the updated data for the post, including updated vote count
+  .then( () => {
+      return Post.findOne({
+          where: {
+              id: req.body.post_id
+          },
+          attributes: [
+              'id',
+              'post_url',
+              'title',
+              'created_at',
+              // use a raw MySQL query to get the vote count, because votes are in a separate table, so findAndCountAll() will not work
+              [
+                  sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
+                  'vote_count'
+              ]
+          ]
+      })
+  })
+  // return the data, or an error if one occurs
+  .then(dbPostData => res.json(dbPostData))
+  .catch(err => res.json(err))
 });
 
 router.put('/:id', (req, res) => {
